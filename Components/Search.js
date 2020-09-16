1b5d862cb0d998e22ad35736af46065861900677
kouzzi010 <kouzzibrhm@gmail.com>
2020-09-16 20:34:47 +0100

    On a crèè tabnavigator

--- Components/Search.js
+++ Components/Search.js
@@ -11,8 +11,8 @@ import {
   ActivityIndicator,
 } from "react-native";
 import FilmItem from "./FilmItem";
+import FilmList from "./FilmList";
 import { getFilmsFromApiWithSearchedText } from "../API/TMDBApi";
-import { connect } from "react-redux";
 
 class Search extends React.Component {
   constructor(props) {
@@ -24,6 +24,7 @@ class Search extends React.Component {
       films: [],
       isLoading: false,
     };
+    this._loadFilms = this._loadFilms.bind(this);
   }
 
   _loadFilms() {
@@ -59,11 +60,6 @@ class Search extends React.Component {
     );
   }
 
-  _displayDetailForFilm = (idFilm) => {
-    console.log("Display film with id " + idFilm);
-    this.props.navigation.navigate("FilmDetail", { idFilm: idFilm });
-  };
-
   _displayLoading() {
     if (this.state.isLoading) {
       return (
@@ -84,32 +80,12 @@ class Search extends React.Component {
           onSubmitEditing={() => this._searchFilms()}
         />
         <Button title="Rechercher" onPress={() => this._searchFilms()} />
-        <FlatList
-          data={this.state.films}
-          extraData={this.props.favoritesFilm}
-          // On utilise la prop extraData pour indiquer à notre FlatList que d’autres données doivent être prises en compte si on lui demande de se re-rendre
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => (
-            <FilmItem
-              film={item}
-              // Ajout d'une props isFilmFavorite pour indiquer à l'item d'afficher un 🖤 ou non
-              isFilmFavorite={
-                this.props.favoritesFilm.findIndex(
-                  (film) => film.id === item.id
-                ) !== -1
-                  ? true
-                  : false
-              }
-              displayDetailForFilm={this._displayDetailForFilm}
-            />
-          )}
-          onEndReachedThreshold={0.5}
-          onEndReached={() => {
-            if (this.page < this.totalPages) {
-              // On vérifie également qu'on n'a pas atteint la fin de la pagination (totalPages) avant de charger plus d'éléments
-              this._loadFilms();
-            }
-          }}
+        <FilmList
+          films={this.state.films} // C'est bien le component Search qui récupère les films depuis l'API et on les transmet ici pour que le component FilmList les affiche
+          navigation={this.props.navigation} // Ici on transmet les informations de navigation pour permettre au component FilmList de naviguer vers le détail d'un film
+          loadFilms={this._loadFilms} // _loadFilm charge les films suivants, ça concerne l'API, le component FilmList va juste appeler cette méthode quand l'utilisateur aura parcouru tous les films et c'est le component Search qui lui fournira les films suivants
+          page={this.page}
+          totalPages={this.totalPages} // les infos page et totalPages vont être utile, côté component FilmList, pour ne pas déclencher l'évènement pour charger plus de film si on a atteint la dernière page
         />
         {this._displayLoading()}
       </View>
@@ -140,11 +116,4 @@ const styles = StyleSheet.create({
   },
 });
 
-// On connecte le store Redux, ainsi que les films favoris du state de notre application, à notre component Search
-const mapStateToProps = (state) => {
-  return {
-    favoritesFilm: state.favoritesFilm,
-  };
-};
-
-export default connect(mapStateToProps)(Search);
+export default Search;
